c412f5a1ad65fcb020437bfea3ca99bdfe6ff663
Baba Coder <96061996+manishsinghbhakuni9@users.noreply.github.com>
2025-01-25 00:39:49 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -509,6 +509,166 @@ responsibleClick.addEventListener("click", ()=>{
 
 }
 
+
+const home = document.querySelector('#home');
+if(home){
+  const newsBox = document.getElementById("newsBox");
+  const newsImg = document.getElementById("newsImg");
+  const newsText = document.getElementById("newsText");
+  
+
+  const slotsBox = document.querySelector("#slotsBox");
+  const slotsImg = document.querySelector("#slotsImg");
+  const slotsText = document.querySelector("#slotsText");
+
+  const tableGamesBox = document.querySelector("#tableGamesBox");
+  const tableGamesImg = document.querySelector("#tableGamesImg");
+  const tableGamesText = document.querySelector("#tableGamesText");
+  
+  const freeGamesBox = document.querySelector("#freeGamesBox");
+  const freeGamesImg = document.querySelector("#freeGamesImg");
+  const freeGamesText = document.querySelector("#freeGamesText");
+  
+  const bonusBox = document.querySelector("#bonusBox");
+  const bonusImg = document.querySelector("#bonusImg");
+  const bonusTexts = document.querySelector("#bonusTexts");
+  
+  const casinoBox = document.querySelector("#casinoBox");
+  const casinoImg = document.querySelector("#casinoImg");
+  const casinoText = document.querySelector("#casinoText");
+  
+  if (slotsBox && slotsImg) {
+    slotsBox.addEventListener('mouseenter', () => {
+      slotsImg.src = "img/home/Slot-w.png"; 
+    });
+    slotsBox.addEventListener('mouseout', () => {
+      slotsImg.src = "img/home/slots.png"; 
+    });
+    slotsImg.addEventListener('mouseenter', () => {
+      slotsImg.src = "img/home/Slot-w.png"; 
+    });
+    slotsImg.addEventListener('mouseout', () => {
+      slotsImg.src = "img/home/slots.png"; 
+    });
+    slotsText.addEventListener('mouseenter', () => {
+      slotsImg.src = "img/home/Slot-w.png"; 
+    });
+    slotsText.addEventListener('mouseout', () => {
+      slotsImg.src = "img/home/slots.png"; 
+    });
+  }
+
+  if (tableGamesBox && tableGamesImg) {
+    tableGamesBox.addEventListener('mouseenter', () => {
+      tableGamesImg.src = "img/home/Table-w.png"; 
+    });
+    tableGamesBox.addEventListener('mouseout', () => {
+      tableGamesImg.src = "img/home/Table_Games.png"; 
+    });
+    tableGamesImg.addEventListener('mouseenter', () => {
+      tableGamesImg.src = "img/home/Table-w.png"; 
+    });
+    tableGamesImg.addEventListener('mouseout', () => {
+      tableGamesImg.src = "img/home/Table_Games.png"; 
+    });
+    tableGamesText.addEventListener('mouseenter', () => {
+      tableGamesImg.src = "img/home/Table-w.png"; 
+    });
+    tableGamesText.addEventListener('mouseout', () => {
+      tableGamesImg.src = "img/home/Table_Games.png"; 
+    });
+  }
+
+  if (freeGamesBox && freeGamesImg) {
+    freeGamesBox.addEventListener('mouseenter', () => {
+      freeGamesImg.src = "img/home/Freegames-w.png"; 
+    });
+    freeGamesBox.addEventListener('mouseout', () => {
+      freeGamesImg.src = "img/home/FreeGames.png"; 
+    });
+    freeGamesImg.addEventListener('mouseenter', () => {
+      freeGamesImg.src = "img/home/Freegames-w.png"; 
+    });
+    freeGamesImg.addEventListener('mouseout', () => {
+      freeGamesImg.src = "img/home/FreeGames.png"; 
+    });
+
+    freeGamesText.addEventListener('mouseenter', () => {
+      freeGamesImg.src = "img/home/Freegames-w.png"; 
+    });
+    freeGamesText.addEventListener('mouseout', () => {
+      freeGamesImg.src = "img/home/FreeGames.png"; 
+    });
+  }
+
+  if (bonusBox && bonusImg) {
+    bonusBox.addEventListener('mouseenter', () => {
+      bonusImg.src = "img/home/Bonus_w.png"; 
+    });
+    bonusBox.addEventListener('mouseout', () => {
+      bonusImg.src = "img/home/Bonus.png"; 
+    });
+    bonusImg.addEventListener('mouseenter', () => {
+      bonusImg.src = "img/home/Bonus_w.png"; 
+    });
+    bonusImg.addEventListener('mouseout', () => {
+      bonusImg.src = "img/home/Bonus.png"; 
+    });
+
+    bonusTexts.addEventListener('mouseenter', () => {
+      bonusImg.src = "img/home/Bonus_w.png"; 
+    });
+    bonusTexts.addEventListener('mouseout', () => {
+      bonusImg.src = "img/home/Bonus.png"; 
+    });
+  }
+
+  if (casinoBox && casinoImg) {
+    casinoBox.addEventListener('mouseenter', () => {
+      casinoImg.src = "img/home/Casino_w.png"; 
+    });
+    casinoBox.addEventListener('mouseout', () => {
+      casinoImg.src = "img/home/casino.png"; 
+    });
+    casinoImg.addEventListener('mouseenter', () => {
+      casinoImg.src = "img/home/Casino_w.png"; 
+    });
+    casinoImg.addEventListener('mouseout', () => {
+      casinoImg.src = "img/home/casino.png"; 
+    });
+
+    casinoText.addEventListener('mouseenter', () => {
+      casinoImg.src = "img/home/Casino_w.png"; 
+    });
+    casinoText.addEventListener('mouseout', () => {
+      casinoImg.src = "img/home/casino.png"; 
+    });
+  }
+
+  if (newsBox && newsImg) {
+    newsBox.addEventListener('mouseenter', () => {
+      newsImg.src = "img/home/Newz-w.png"; 
+    });
+    newsBox.addEventListener('mouseout', () => {
+      newsImg.src = "img/home/news.png"; 
+    });
+    newsImg.addEventListener('mouseenter', () => {
+      newsImg.src = "img/home/Newz-w.png"; 
+    });
+    newsImg.addEventListener('mouseout', () => {
+      newsImg.src = "img/home/news.png"; 
+    });
+
+    newsText.addEventListener('mouseenter', () => {
+      newsImg.src = "img/home/Newz-w.png"; 
+    });
+    newsText.addEventListener('mouseout', () => {
+      newsImg.src = "img/home/news.png"; 
+    });
+  }
+}
+
+
 if(!review){
   let faqBtn = document.querySelector("#faqBtn");
   let faqSect = document.querySelector("#faqSect");
@@ -527,6 +687,3 @@ if(!review){
     }
   })
 }
-
-
-
